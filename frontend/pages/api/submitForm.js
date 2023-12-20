da1c119fe6638b31372be27113cd6550462847e0
SahilNalavade <sahilnalavade25@gmail.com>
2023-12-20 13:59:26 +0530

    url update

--- frontend/pages/api/submitForm.js
+++ frontend/pages/api/submitForm.js
@@ -1,4 +1,4 @@
-const backendUrl = 'https://yoga-web-one.vercel.app/api/submitForm'; // Assuming /api/submitForm is your backend endpoint
+const backendUrl = 'https://yoga-web-omega.vercel.app/api/submitForm'; // Assuming /api/submitForm is your backend endpoint
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {

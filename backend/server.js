35f7406cb64bddc2ff47d6dac3856f349c9a6a9a
SahilNalavade <sahilnalavade25@gmail.com>
2023-12-20 08:57:17 +0530

    backend check

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,35 @@
-// server.js
-const express = require('express');
-const pgp = require('pg-promise')();
-const app = express();
-const port = process.env.PORT || 3002;
+// // server.js
+// const express = require('express');
+// const pgp = require('pg-promise')();
+// const app = express();
+// const port = process.env.PORT || 3002;
 
-// Replace the following configuration with your actual database credentials and connection details
-const db = pgp({
-  user: 'postgres',
-  password: 'Admin0272',
-  host: 'database-3.cb86aoug6oz9.us-east-1.rds.amazonaws.com',
-  port: 5432,
-  database: 'demo',
-  ssl: true,
-});
+// // Replace the following configuration with your actual database credentials and connection details
+// const db = pgp({
+//   user: 'postgres',
+//   password: 'Admin0272',
+//   host: 'database-3.cb86aoug6oz9.us-east-1.rds.amazonaws.com',
+//   port: 5432,
+//   database: 'demo',
+//   ssl: true,
+// });
 
 
-app.use(express.json());
+// app.use(express.json());
 
-// Endpoint to get form data
-app.get('/api/getFormData', async (req, res) => {
-  try {
-    // Update the query to include LIMIT 1
-    const formData = await db.one('SELECT * FROM yoga_registration ORDER BY registration_date DESC LIMIT 1');
+// // Endpoint to get form data
+// app.get('/api/getFormData', async (req, res) => {
+//   try {
+//     // Update the query to include LIMIT 1
+//     const formData = await db.one('SELECT * FROM yoga_registration ORDER BY registration_date DESC LIMIT 1');
     
-    res.status(200).json(formData);
-  } catch (error) {
-    console.error('Error fetching form data:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+//     res.status(200).json(formData);
+//   } catch (error) {
+//     console.error('Error fetching form data:', error);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
 
-app.listen(port, () => {
-  console.log(`Backend server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Backend server is running on port ${port}`);
+// });
--- frontend/pages/api/getFormData.js
+++ frontend/pages/api/getFormData.js
@@ -1,7 +1,7 @@
-// getFormData.js
-export default async function handler(req, res) {
-    const response = await fetch('http://localhost:3002/api/getFormData');
-    const data = await response.json();
-    res.status(200).json(data);
-  }
+// // getFormData.js
+// export default async function handler(req, res) {
+//     const response = await fetch('http://localhost:3002/api/getFormData');
+//     const data = await response.json();
+//     res.status(200).json(data);
+//   }
   
